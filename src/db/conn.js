faa9de539418dbe955bb287404d99023692c88c0
Rehan Mansoori <90987707+rhn-ryan@users.noreply.github.com>
2023-10-23 22:36:05 +0530

    Header and footer added

--- src/db/conn.js
+++ src/db/conn.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 const MONGODB_URI = process.env.ATLAS_URI
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/loyalRegisteration",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/loyalRegisteration",
 {
     useUnifiedTopology:true,
     useNewUrlParser:true,
